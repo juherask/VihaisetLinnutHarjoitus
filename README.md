31d2e85aafd33e3a2a0846b8d97821ea043dbb37
juherask <jussi.rasku@jyu.fi>
2014-10-04 11:35:56 +0300

    - Lisätty harjoituksen koodit ja ohjeet.

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ VihaisetLinnut
 ==============
 
 VihaisetLinnut harjoitus, jossa tehdään C#-kielellä ja Jypeli-kirjastolla Angry Birds klooni seurailemalla kooditiedoston kommenteissa olevia ohjeita.
+
+## Käyttöohje ##
+
+1.  Lataa tämä säiliö (reposotory) zip-pakettina omalle koneellesi. `Download ZIP` -latauspainike on tämän Github-sivun oikeassa laidassa hieman tätä riviä ylempänä.
+2.  Pura `.zip`-paketti ja avaa puretusta kansiosta Visual C#:n solution tiedosto `VihaisetLinnut.sln`.
+3.  Avaa `VihaisetLinnut.cs` tiedosto muokattavaksi tuplaklikkaamalla sitä Visual C#:ssa.
+4.  Lue tiedoston alussa kommenteissa oleva ohjeistus ja seuraa sitä.
+5.  Voit kokoajan testata peliäsi painamalla `F5` Visual C#:ssä.
+
+## Lisäohjeita ja vinkkejä ##
+
+JyPeli-wikin ohjeet ovat hyödyksi harjoitusta tehtäessä:
+https://trac.cc.jyu.fi/projects/npo/wiki/KirjastonOhjeet
---
+++ VihaisetLinnut/VihaisetLinnut/VihaisetLinnutValmis.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Effects;
+using Jypeli.Widgets;
+
+public class VihaisetLinnutValmis : PhysicsGame
+{
+    PhysicsObject lintu;
+    PhysicsObject possu;
+
+    public override void Begin()
+    {
+        lintu = new PhysicsObject(40, 40, Shape.Circle);
+        lintu.Color = Color.Red;
+        lintu.X = -400;
+        lintu.Y = -350;
+        lintu.IgnoresGravity = true;
+        Add(lintu);
+
+        PhysicsObject maa = Level.CreateBottomBorder();
+
+        possu = new PhysicsObject(40, 40, Shape.Circle);
+        possu.Color = Color.Green;
+        possu.X = +400;
+        possu.Y = -350;
+        Add(possu);
+        AddCollisionHandler(possu, PossuunOsuu);
+
+        Gravity = new Vector(0, -500);
+        LisaaKentta();
+
+        IsMouseVisible = true;
+        Mouse.Listen(MouseButton.Left, ButtonState.Released, AmmuLintu, "Ammu lintu hiirellä");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+
+    void LisaaKentta() {
+        PhysicsObject palikka1 = new PhysicsObject(120, 20);
+        palikka1.Position = new Vector(+400, -250);
+        Add(palikka1);
+        PhysicsObject palikka2 = new PhysicsObject(20, 100);
+        palikka2.Position = new Vector(+440, -350);
+        Add(palikka2);
+        PhysicsObject palikka3 = new PhysicsObject(20, 100);
+        palikka3.Position = new Vector(+360, -350);
+        Add(palikka3);
+    }
+
+    void AmmuLintu()
+    {
+        lintu.Hit((Mouse.PositionOnScreen - lintu.Position) * -4.0);
+        lintu.IgnoresGravity = false;
+    }
+
+    void PossuunOsuu(PhysicsObject tormaaja, PhysicsObject kohde)
+    {
+        if (tormaaja.Velocity.Magnitude > 200 || kohde.Velocity.Magnitude > 200)
+            possu.Destroy();
+    }
+}
